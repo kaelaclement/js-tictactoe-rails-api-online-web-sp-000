bd1a1d5071e03778d55df61e9b7b6b8e885771ac
kaelaclement <stephanie.kaela@gmail.com>
2019-04-01 14:46:30 +0100

    Done.

--- app/assets/javascripts/tictactoe.js
+++ app/assets/javascripts/tictactoe.js
@@ -1,1 +1,10 @@
 // Code your JavaScript / jQuery solution here
+function player() {
+  let player;
+  if (turn % 2 == 0 || turn == 0) {
+    player = 'X';
+  } else if (turn %2 == 1 || turn == 1) {
+    player = 'O';
+  };
+  return player;
+};
