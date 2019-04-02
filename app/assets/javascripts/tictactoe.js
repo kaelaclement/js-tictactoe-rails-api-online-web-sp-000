1e7c972b51c6ce9a8f5b2afcf244b39bdcaebf81
kaelaclement <stephanie.kaela@gmail.com>
2019-04-02 22:13:49 +0100

    Done.

--- app/assets/javascripts/tictactoe.js
+++ app/assets/javascripts/tictactoe.js
@@ -19,20 +19,32 @@ function setMessage(message) {
 };
 
 function checkWinner() {
-  // select each possible win row
+  const winningCombinations = [
+  [0, 1, 2], // top –––
+  [3, 4, 5], // middle –––
+  [6, 7, 8], // bottom –––
+  [0, 3, 6], // left |
+  [1, 4, 7], // middle |
+  [2, 5, 8], // right |
+  [0, 4, 8], // \ diagonal
+  [2, 4, 6] // / diagonal
+  ]
+  let gameState = [];
 
-  // but not like this
-  const $topRow = $("td[data-y='0']");
-  // const $midRow = $("td[data-y='1']");
-  // const $botRow = $("td[data-y='2']");
-  // const $leftCol = $("td[data-x='0']");
-  // const $midCol = $("td[data-x='1']");
-  // const $rightCol = $("td[data-x='2']");
-  // const $majorDiag = $("td[data-x='0']");
+  $("td").each(function (i, el) {
+    gameState.push(el.innerText);
+  });
 
   // check for a winning pattern
 
-  // return if someone won
-
-  // use setMessage to return who won
+  winningCombinations.forEach(function (winArr) {
+    if (gameState[winArr[0]] === 'X' || gameState[winArr[0]] === 'O') {
+      if (gameState[winArr[0]] === gameState[winArr[1]] && gameState[winArr[1]] === gameState[winArr[2]]) {
+        setMessage('Player ' + gameState[winArr[0]] + ' Won!');
+        return true;
+      }
+    } else {
+      return false;
+    }
+  });
 };
